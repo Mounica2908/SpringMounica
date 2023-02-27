5cd0d57e600dccf13d856fd33d9d216830193d89
Mounica2908 <126123893+Mounica2908@users.noreply.github.com>
2023-02-27 17:52:30 -0500

    Add files via upload

---
+++ spring5webApp/spring5webApp/src/test/java/mounica/sringframework/spring5webApp/Spring5webAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mounica.sringframework.spring5webApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
